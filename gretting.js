f141c3876dc76764d7af2601d011d7add8d23cf3
CHOIDONGGYUN <chdg2851@gmail.com>
2020-10-21 21:59:41 +0900

    1021_gretting

---
+++ gretting.js
@@ -0,0 +1,46 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerHTML = `Hello ${text}`;
+}
+
+function handleSumbit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue); 
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+
+    form.addEventListener("submit", handleSumbit);
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    // NOT User
+    if ( currentUser === null ) {
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
